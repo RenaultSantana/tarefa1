267cf70a73678026d1b441631b9f96539086c09a
RenaultSantana <renaultsbrito@gmail.com>
2025-06-03 21:18:20 -0300

    Update Checkout.js

---
+++ src/pages/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Checkout() {
+  return (
+    <div>
+      <h1>Página de Checkout</h1>
+      <p>Aqui você pode realizar o pagamento e finalizar sua compra.</p>
+    </div>
+  );
+}
+
+export default Checkout;
