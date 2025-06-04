2f33d7f9c4d838a21079393ed206277fd4586295
RenaultSantana <renaultsbrito@gmail.com>
2025-06-03 21:17:50 -0300

    Update Carrinho.js

---
+++ src/pages/Carrinho.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Carrinho() {
+  return (
+    <div>
+      <h1>Página do Carrinho</h1>
+      <p>Aqui você pode visualizar os itens no seu carrinho.</p>
+    </div>
+  );
+}
+
+export default Carrinho;
